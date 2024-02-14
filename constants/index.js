e5f32f5cbdcbbc192f6c1a41d253c9d2a26a5495
OksanaMosk <oks-m3@ukr.net>
2024-02-14 23:24:41 +0200

    auth1

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,11 @@
 const EMAIL_REGEX = require('./emailRegex');
 const DRINKS_CATEGORY = require('./drinksCategory');
 const GLASSES = require('./glasses');
+const DATE_REGEX = require('./dateRegex');
 
 module.exports = {
   EMAIL_REGEX,
+  DATE_REGEX,
   DRINKS_CATEGORY,
   GLASSES,
 };
--- models/user.js
+++ models/user.js
@@ -1,18 +1,83 @@
 const Joi = require('joi');
 const { Schema, model } = require('mongoose');
 const { handleMongooseError } = require('../helpers');
-const { EMAIL_REGEX } = require('../constants');
+const { EMAIL_REGEX, DATE_REGEX } = require('../constants');
 
 // mongoose schema
-const userSchema = new Schema({}, { versionKey: false, timestamps: true });
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: String,
+      match: DATE_REGEX,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: EMAIL_REGEX,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    token: {
+      type: String,
+      default: '',
+    },
+    avatarURL: {
+      type: String,
+      require: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 // mongoose error handler
 userSchema.post('save', handleMongooseError);
 
 // Joi schemas
 
+const signupSchema = Joi.object({
+  name: Joi.string().required(),
+  dateOfBirth: Joi.string().pattern(DATE_REGEX).required(),
+  email: Joi.string().pattern(EMAIL_REGEX).required().messages({
+    'string.base': `email should be a type of 'string'`,
+    'string.empty': `email cannot be an empty field`,
+    'any.required': `missing required email field`,
+    'string.pattern.base': 'wrong email format',
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.base': `password should be a type of 'string'`,
+    'string.empty': `password cannot be an empty field`,
+    'any.required': `missing required password field`,
+  }),
+});
+
+const signinSchema = Joi.object({
+  email: Joi.string().pattern(EMAIL_REGEX).required().messages({
+    'string.base': `email should be a type of 'string'`,
+    'string.empty': `email cannot be an empty field`,
+    'any.required': `missing required email field`,
+    'string.pattern.base': 'wrong email format',
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.base': `password should be a type of 'string'`,
+    'string.empty': `password cannot be an empty field`,
+    'any.required': `missing required password field`,
+  }),
+});
+
 // group all Joi schemas
-const schemas = {};
+const schemas = {
+  signupSchema,
+  signinSchema,
+};
 
 // create model for user
 const User = model('user', userSchema);
