60ce4b9ed64591ad6586adc6bff9299cb802e26e
Nataliya-Viktorivna-Pashchenko <130203943+Nataliya-Viktorivna-Pashchenko@users.noreply.github.com>
2024-02-15 23:06:21 +0100

    Add-Get-Drink-Owner

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const { controllerWrapper } = require('../decorators');
 
 const listCategory = require('./filters/categories');
 const updatedSubscribe = require('./subscribes/subscribe');
+const getDrinkOwner = require('./drinks/getDrinksOwner')
 
 module.exports = {
   updatedSubscribe: controllerWrapper(updatedSubscribe),
   listCategory: controllerWrapper(listCategory),
+  getDrinkOwner: controllerWrapper(getDrinkOwner),
 };
--- routes/drinks.js
+++ routes/drinks.js
@@ -6,6 +6,10 @@ const { validateBody, authenticate, isValidId } = require('../middlewares');
 
 const jsonParser = express.json();
 
+const Ctrl = require('../controllers');
+
+
 // drinks routes
+router.get('/own', Ctrl.getDrinkOwner)
 
 module.exports = router;
