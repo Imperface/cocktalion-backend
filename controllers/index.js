6b63089a18e00488b10fcea541113255a605e403
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2024-02-15 20:16:11 +0200

    categories

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const { listCategory } = require('./filters');
+
+module.exports = {
+  listCategory,
+};
--- routes/filters.js
+++ routes/filters.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const ctrl = require('../controllers');
+
 const { authenticate } = require('../middlewares');
 
 // filters routes
+router.get('/categories', authenticate, ctrl.listCategory);
+
+router.get('/ingredients', authenticate);
+
+router.get('/glasses', authenticate);
 
 module.exports = router;
