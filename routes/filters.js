f5fa54b3e74eda06e81a09147229092539912019
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2024-02-15 20:59:38 +0200

    C

--- routes/filters.js
+++ routes/filters.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const ctrl = require('../controllers');
+const Ctrl = require('../controllers');
 
 const { authenticate } = require('../middlewares');
 
 // filters routes
-router.get('/categories', authenticate, ctrl.listCategory);
+router.get('/categories', authenticate, Ctrl.listCategory);
 
 router.get('/ingredients', authenticate);
 
