8abd132a4a26345e21dd5e7823ce15e801a002b5
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2024-02-16 16:24:06 +0200

    Filter glasses

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const { controllerWrapper } = require('../decorators');
 
 const listCategory = require('./filters/categories');
+const listGlasses = require('./filters/glasses');
 const updatedSubscribe = require('./subscribes/subscribe');
 const getDrinkById = require('./drinks/getDrinkById.js');
 const getCurrent = require('./users/getCurrent');
@@ -8,6 +9,7 @@ const getCurrent = require('./users/getCurrent');
 module.exports = {
   updatedSubscribe: controllerWrapper(updatedSubscribe),
   listCategory: controllerWrapper(listCategory),
+  listGlasses: controllerWrapper(listGlasses),
   getDrinkById: controllerWrapper(getDrinkById),
   getCurrent: controllerWrapper(getCurrent),
 };
