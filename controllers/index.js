f0a30f641520a4cd82f388ba312714ef3db8ae15
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-02-16 17:47:05 +0200

    add re-export

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,7 @@ const listGlasses = require('./filters/glasses');
 const updatedSubscribe = require('./subscribes/subscribe');
 const getDrinkById = require('./drinks/getDrinkById.js');
 const getCurrent = require('./users/getCurrent');
+const getFavoriteDrinks = require('./drinks/getFavoriteDrinks');
 
 module.exports = {
   updatedSubscribe: controllerWrapper(updatedSubscribe),
@@ -12,4 +13,5 @@ module.exports = {
   listGlasses: controllerWrapper(listGlasses),
   getDrinkById: controllerWrapper(getDrinkById),
   getCurrent: controllerWrapper(getCurrent),
+  getFavoriteDrinks: controllerWrapper(getFavoriteDrinks),
 };
