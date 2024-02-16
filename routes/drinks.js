85fe906b9b9f7adacb2d6152e8019afd15ed475c
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-02-16 17:45:18 +0200

    add route getFavoriteDrinks

--- routes/drinks.js
+++ routes/drinks.js
@@ -9,6 +9,7 @@ const { validateBody, authenticate, isValidId } = require('../middlewares');
 const jsonParser = express.json();
 
 // drinks routes
+router.get('/', authenticate, jsonParser, Ctrl.getFavoriteDrinks);
 router.get('/:id', isValidId, Ctrl.getDrinkById);
 
 module.exports = router;
