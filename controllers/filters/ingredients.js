205e1ef07e7be7314743474f057e353db0f578f9
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2024-02-17 13:23:57 +0200

    с

--- controllers/filters/ingredients.js
+++ controllers/filters/ingredients.js
@@ -1,9 +1,12 @@
 const { Ingredient } = require('../../models/ingredient');
 
 const listIngredients = async (req, res) => {
-  const ingredients = await Ingredient.find({});
+  const ingredients = await Ingredient.find(
+    {},
+    '_id title ingredientThumb thumb-medium thumb-small'
+  );
 
-  res.status(200).json({ ingredients: ingredients });
+  res.status(200).json({ ingredients });
 };
 
 module.exports = listIngredients;
