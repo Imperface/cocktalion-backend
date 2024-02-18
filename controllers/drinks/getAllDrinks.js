3eef88b020ed3b2face43d58e90e48c3f09879fc
Imperface <kovzhenko.ua@gmail.com>
2024-02-18 02:15:44 +0200

    update get all drinks

--- controllers/drinks/getAllDrinks.js
+++ controllers/drinks/getAllDrinks.js
@@ -1,18 +1,14 @@
 const { Drink } = require('../../models/drink');
 
-const defaultDocNumber = 3;
-
 const getAllDrinks = async (req, res) => {
-  const isNumberSet = Boolean(req.query.number_of_every);
+  let { number_of_every = 3 } = req.query;
 
-  const docNumber = isNumberSet
-    ? Number(req.query.number_of_every)
-    : defaultDocNumber;
+  number_of_every = Number(number_of_every);
 
   const result = await Drink.aggregate([
     { $sort: { category: 1 } },
     { $group: { _id: '$category', recipes: { $push: '$$ROOT' } } },
-    { $project: { drinks: { $slice: ['$recipes', docNumber] } } },
+    { $project: { drinks: { $slice: ['$recipes', number_of_every] } } },
     { $limit: 4 },
   ]);
 
