cb9f4523fa8716413ae19766473fe9e3f9dd1002
Imperface <kovzhenko.ua@gmail.com>
2024-02-18 20:48:13 +0200

    fix get popular

--- controllers/drinks/getDrinksPopular.js
+++ controllers/drinks/getDrinksPopular.js
@@ -1,38 +1,46 @@
-const {Drink} = require ('../../models/drink');
-const {isAdult} = require('../../helpers')
+const { Drink } = require('../../models/drink');
+const { isAdult } = require('../../helpers');
 
-const getDrinksPopular = async(req, res) => {
-
-const findPopularDrinks = (arr) => {
-    const drinksFavorite = arr.filter(drink => {
+const getDrinksPopular = async (req, res) => {
+  const findPopularDrinks = arr => {
+    const drinksFavorite = arr
+      .filter(drink => {
         if (drink.favorites.length !== 0) {
-            return true
+          return true;
         }
-    })
-    .sort(drink = (a, b) => {
-        if (a.favorites.length < b.favorites.length) {
-            return 1
-        }
-        if (a.favorites.length > b.favorites.length) {
-            return -1
-        } 
-        return 0   
-        } )
-    .slice(0, 4);
-       
-res.send(drinksFavorite);
-}
-    
-    const chekAdult = isAdult(req.user.dateOfBirth);
-    
-    if (chekAdult === false){
-        const drinksAll = await Drink.find({alcoholic: "Non alcoholic"});
+      })
+      .sort(
+        (drink = (a, b) => {
+          if (a.favorites.length < b.favorites.length) {
+            return 1;
+          }
+          if (a.favorites.length > b.favorites.length) {
+            return -1;
+          }
+          return 0;
+        })
+      )
+      .slice(0, 4);
 
-   findPopularDrinks(drinksAll);
-} 
-const drinksAll = await Drink.find();  
-findPopularDrinks(drinksAll);
-  
-}
+    res.status(200).json({ drinks: drinksFavorite });
+  };
+
+  const chekAdult = isAdult(req.user.dateOfBirth);
+
+  if (chekAdult === false) {
+    const drinksAll = await Drink.find(
+      { alcoholic: 'Non alcoholic' },
+      '_id drink shortDescription drinkThumb favorites'
+    );
+
+    findPopularDrinks(drinksAll);
+    return;
+  }
+  const drinksAll = await Drink.find(
+    {},
+    '_id drink shortDescription drinkThumb favorites'
+  );
+  findPopularDrinks(drinksAll);
+};
 
 module.exports = getDrinksPopular;
