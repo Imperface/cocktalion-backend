651eb309e1f6b451f498170a6620be0d9478bde7
Imperface <kovzhenko.ua@gmail.com>
2024-02-19 21:26:21 +0200

    populate addition info to getDrinkById

--- controllers/drinks/getDrinkById.js
+++ controllers/drinks/getDrinkById.js
@@ -2,7 +2,10 @@ const { Drink } = require('../../models/drink');
 const { HttpError } = require('../../helpers');
 
 const getDrinkById = async (req, res) => {
-  const drink = await Drink.findById(req.params.id);
+  const drink = await Drink.findById(req.params.id).populate(
+    'ingredients.ingredientId',
+    'ingredientThumb thumb-medium thumb-small'
+  );
 
   if (drink === null) {
     throw HttpError(404, 'Not found');
