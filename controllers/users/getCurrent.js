6f2d486a342cc84decb2559e5b6b12026114b199
Imperface <kovzhenko.ua@gmail.com>
2024-02-23 05:41:37 +0200

    add _id to current response

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,13 +1,13 @@
 const { HttpError } = require('../../helpers');
 
 const getCurrent = async (req, res) => {
-  const { name, email, dateOfBirth, avatarURL } = req.user;
+  const { name, email, dateOfBirth, avatarURL, _id } = req.user;
 
   if (!email) {
     throw HttpError(404, 'Not found');
   }
 
-  res.status(200).json({ name, email, dateOfBirth, avatarURL });
+  res.status(200).json({ name, email, dateOfBirth, avatarURL, _id });
 };
 
 module.exports = getCurrent;
