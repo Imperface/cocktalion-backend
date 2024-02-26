2daadbab608406f561d1b21f039a081444f369db
Anton Vasyliev <anton12vasyliev@gmail.com>
2024-02-26 12:32:58 +0000

    fix/adult-drinks

--- controllers/drinks/getAllDrinks.js
+++ controllers/drinks/getAllDrinks.js
@@ -1,11 +1,19 @@
 const { Drink } = require('../../models/drink');
+const isAdult = require('../../helpers/isAdult');
 
 const getAllDrinks = async (req, res) => {
   let { number_of_every = 3 } = req.query;
 
   number_of_every = Number(number_of_every);
 
+  const { dateOfBirth } = req.user;
+
+  const isUserAdult = isAdult(dateOfBirth);
+
+  const alcoholicFilter = isUserAdult ? {} : { alcoholic: 'Non alcoholic' };
+
   const result = await Drink.aggregate([
+    { $match: alcoholicFilter },
     { $sort: { category: 1 } },
     { $group: { _id: '$category', recipes: { $push: '$$ROOT' } } },
     { $project: { drinks: { $slice: ['$recipes', number_of_every] } } },
